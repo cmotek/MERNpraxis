aa331e309a0f029775c0d946127a555da3a69e89
unknown <cmotek1122@gmail.com>
2022-02-15 20:45:04 -0600

    updateupdate20152022

--- MERNProject/src/user/pages/Users.js
+++ MERNProject/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users Works!</h2>;
+    const USERS = [
+        {
+            id: 'u1', 
+            name: 'Max Scwarz', 
+            image: 'https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80', 
+            places: 3
+        }
+    ];
+
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
